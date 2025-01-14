7dbf92f0cfef2693cec56743089ca02d1e79cbe9
eunwoo <aufcl4858@naver.com>
2025-01-14 23:39:40 +0900

    문제추가

---
+++ src/11727/11727.kt
@@ -0,0 +1,25 @@
+package `11727`
+
+lateinit var dp: Array<Int>
+var N = 0
+
+fun main() {
+    N = readln().toInt()
+    dp = Array(N + 1) { 0 }
+    recur(0)
+    println(dp[0] % 10007)
+}
+
+fun recur(index: Int): Int {
+    if (index == N) {
+        return 1
+    }
+    if (index > N) {
+        return 0
+    }
+    if (dp[index] != 0) {
+        return dp[index]
+    }
+    dp[index] = recur(index + 1) + recur(index + 2) + recur(index + 2)
+    return dp[index]
+}
