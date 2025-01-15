0e14bf6a6880a0e429f1fb7d47b1e9556bbf18d4
eunwoo <eunwoo@finda.co.kr>
2025-01-15 09:24:17 +0900

    문제 추가

--- src/11727/11727.kt
+++ src/11727/11727.kt
@@ -5,9 +5,10 @@ var N = 0
 
 fun main() {
     N = readln().toInt()
-    dp = Array(N + 1) { 0 }
+    dp = Array(N) { 0 }
+
     recur(0)
-    println(dp[0] % 10007)
+    println(dp[0])
 }
 
 fun recur(index: Int): Int {
@@ -20,6 +21,6 @@ fun recur(index: Int): Int {
     if (dp[index] != 0) {
         return dp[index]
     }
-    dp[index] = recur(index + 1) + recur(index + 2) + recur(index + 2)
+    dp[index] = (recur(index + 1) + recur(index + 2) + recur(index + 2)) % 10007
     return dp[index]
 }
