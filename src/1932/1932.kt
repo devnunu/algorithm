d9124acab330ddd68d9d04ea2174cba914fb293e
eunwoo <eunwoo@finda.co.kr>
2025-01-16 08:45:01 +0900

    문제 추가

--- src/1932/1932.kt
+++ src/1932/1932.kt
@@ -1,4 +1,32 @@
 package `1932`
 
-class `1932` {
+val list = mutableListOf<List<Int>>()
+lateinit var dp: Array<Array<Int>>
+
+fun main() {
+    val N = readln().toInt()
+    repeat(N) {
+        val array = Array(N) { -1 }
+        val inputList = readln().split(" ").map { it.toInt() }
+        array.mapIndexed { index, i ->
+            array[index] = inputList.getOrNull(index) ?: -1
+        }
+        list.add(array.toList())
+    }
+
+    dp = Array(N + 1) { Array(N + 1) { -1 } }
+
+    println(recur(N, 0, 0))
 }
+
+fun recur(N: Int, x: Int, y: Int): Int {
+    if (x == N - 1) {
+        return list[x][y]
+    }
+    if (dp[x][y] != -1) {
+        return dp[x][y]
+    }
+    dp[x][y] = Math.max(recur(N, x + 1, y), recur(N, x + 1, y + 1)) + list[x][y]
+    return dp[x][y]
+}
+
--- src/2579/2579.kt
+++ src/2579/2579.kt
@@ -1,4 +1,39 @@
 package `2579`
 
-class `2579` {
+var N = 0
+lateinit var dp: Array<Long>
+val list = mutableListOf<Long>()
+
+fun main() {
+    N = readln().toInt()
+    list.add(0)
+    repeat(N) {
+        list.add(readln().toLong())
+    }
+    dp = Array(N + 1) { 0 }
+
+    recur(0, 0)
+    println(dp.contentDeepToString())
+}
+
+fun recur(index: Int, seqCount: Int): Long {
+    if (seqCount == 3) {
+        return 0
+    }
+    if (index == N) {
+        return list[index]
+    }
+    if (index > N) {
+        return 0
+    }
+
+    if (dp[index] != 0L) {
+        return dp[index]
+    }
+
+    dp[index] = list[index] + Math.max(
+        recur(index + 1, seqCount + 1),
+        recur(index + 2, 1)
+    )
+    return dp[index]
 }
